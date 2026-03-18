1f0b858321cdeee765e695a212e3bec216873dff
Iudhi <iudhicunhasantos0@gmail.com>
2026-03-17 21:07:54 -0300

    init: inicio do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,6 +7,6 @@ public class Principal {
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base:"));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura:"));
         area = (base * altura) / 2.0;
-        JOptionPane.showMessageDialog(null,"A área é:" + area);
+        JOptionPane.showMessageDialog(null,"A área é:" + area); 
     }
 }
